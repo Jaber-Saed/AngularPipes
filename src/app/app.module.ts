188b9e65c3743980d3b47d5de7778ea2f7e09897
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-04 13:28:16 +0300

    fix errore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MsComponent } from './ms/ms.component';
-import { JawsadComponent } from './ms/jawsad/jawsad.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      MsComponent,
-      JawsadComponent
    ],
   imports: [
     BrowserModule,
